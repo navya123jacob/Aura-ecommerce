2d1c0d9415fb2045bbd8135dacbe50f71bbc5f6d
navya123jacob <navyatjacob@gmail.com>
2024-01-23 15:48:40 +0530

    bugs

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 // Serve static files from the 'public' directory
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname,'public')))
 
 //for no caching
 const nocache = require('nocache');
