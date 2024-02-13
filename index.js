08cb4e452f42a499edc8f62e98c5a2853c7e9a70
navya123jacob <navyatjacob@gmail.com>
2024-02-13 21:40:24 +0530

    corrections

--- index.js
+++ index.js
@@ -4,19 +4,15 @@ const path = require('path');
 const dotenv = require('dotenv').config();
 const morgan=require('morgan')
 
-// Create an Express application
+
 const app = express();
 
+ mongoose.connect(process.env.MONGO_URL); //process.env is a Node.js global object that provides access to the environment variables. 
 
 
-// Connect to MongoDB
- mongoose.connect(process.env.MONGO_URL); //process.env is a Node.js global object that provides access to the environment variables. Environment variables are key-value pairs that can be set outside of your application and are accessible from within your Node.js application
-// the configuration data stored in config.env (or .env) and accessed using process.env is considered environmental or environment-specific configuration.
-
-// Import userRoute (assuming you have a userRoute file)
 const userRoute = require('./Router/userRoute');
 
-// Import adminRoute
+
 const adminRoute = require('./Router/adminRoute');
 
 //to parse request body
@@ -39,16 +35,16 @@ app.use(session({
     saveUninitialized:true
 }))
 
-// Use the userRoute for requests at the root ('/')
+
 app.use('/', userRoute);
 
-// Use the adminRoute for requests at the root ('/admin')
+
 app.use('/admin', adminRoute);
 
 
 const port = process.env.PORT || 3000;
 
-// Start the server on port 3000
+
 app.listen(port, () => {
   console.log('Listening at http://localhost:3000');
 });
